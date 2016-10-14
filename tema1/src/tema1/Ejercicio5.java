023d5b5119217d9a52e3a541799af0a1aace2987
DaniieR <dam.dromero@institutoserpis.org>
2016-10-14 12:11:12 +0200

    acabado ejercicios

--- tema1/src/tema1/Ejercicio5.java
+++ tema1/src/tema1/Ejercicio5.java
@@ -1,5 +1,30 @@
 package tema1;
 
 public class Ejercicio5 {
+	public static void main(String[] args) {
+		try {
+		    
+		        Process[] procesos = new Process[args.length];
+		        int appsIniciadas = 0;
+		        
+		        for (String cmd:args){
+		    
+		            System.out.println(cmd);
+		    
+		            procesos[appsIniciadas] = Runtime.getRuntime().exec(cmd);
+		            appsIniciadas++;
+		        }
+		        
+		       for(int i=0; i<appsIniciadas; i++)
+		           procesos[i].waitFor();
+		           
+		        
 
+		        System.out.println("todas las aplicaciones finalizadas");
+		        
+		        
+		    } catch (Exception e) {
+		        System.out.println(e.getMessage());
+		    }
+		}
 }
